832687b1c4cbb5f8e0b2329c91114a9709fe6c5c
Peter Kraft <petereliaskraft@gmail.com>
2025-10-29 16:35:55 -0700

    update

--- app/actions.ts
+++ app/actions.ts
@@ -4,14 +4,17 @@ import { DBOSClient } from "@dbos-inc/dbos-sdk";
 import { Pool } from "pg";
 
 export async function runDBOSWorkflow() {
-  console.log("Enqueueing DBOS workflow");
+  console.log("Calling runDBOSWorkflow");
   const databaseURL = process.env.POSTGRES_URL_NON_POOLING?.replace('?sslmode=require', '');
   if (!databaseURL) {
     throw Error("Database URL not defined");
   }
+  console.log("Creating DBOSClient")
   let pool = new Pool({ connectionString: databaseURL });
   const client = await DBOSClient.create({systemDatabaseUrl: databaseURL, systemDatabasePool: pool});
+  console.log("Enqueueing DBOS workflow")
   client.enqueue({
         workflowName: 'exampleWorkflow',
         queueName: 'exampleQueue',})
+  console.log("Workflow enqueued!")
 }
