64317716b1cdfa1c471403626fbd704bd05eea6b
Peter Kraft <petereliaskraft@gmail.com>
2025-11-10 12:42:05 -0800

    dbos

--- app/api/dbos/route.ts
+++ app/api/dbos/route.ts
@@ -1,11 +1,37 @@
+import { DBOS, WorkflowQueue } from "@dbos-inc/dbos-sdk";
 import { waitUntil } from '@vercel/functions';
- 
-async function getBlog() {
-  const res = await fetch('https://example.com/');
-  return res.text();
+
+async function stepOne() {
+  DBOS.logger.info("Step one completed!");
 }
 
+async function stepTwo() {
+  DBOS.logger.info("Step two completed!");
+}
+
+async function exampleFunction() {
+  await DBOS.runStep(() => stepOne(), { name: "stepOne" });
+  await DBOS.runStep(() => stepTwo(), { name: "stepTwo" });
+}
+const exampleWorkflow = DBOS.registerWorkflow(exampleFunction, {
+  name: "exampleWorkflow",
+});
+
+// const exampleQueue = new WorkflowQueue("exampleQueue");
+
 export async function GET(request: Request) {
-    waitUntil(getBlog().then((json) => console.log({ json })));
-    return new Response(`Hello from ${request.url}, I'm a Vercel Function!`);
+  const databaseURL = process.env.POSTGRES_URL_NON_POOLING?.replace(
+    "?sslmode=require",
+    "",
+  );
+  if (!databaseURL) {
+    throw Error("Database URL not defined");
+  }
+  DBOS.setConfig({
+    name: "dbos-vercel-integration",
+    systemDatabaseUrl: databaseURL,
+  });
+  await DBOS.launch();
+  await exampleWorkflow();
+  return new Response(`Hello from ${request.url}, I ran a DBOS workflow!`);
 }
