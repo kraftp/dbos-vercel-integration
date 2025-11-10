83407e7e5736c7e40cd59b3e7720132764a4c24b
Peter Kraft <petereliaskraft@gmail.com>
2025-11-10 13:31:27 -0800

    with client

--- app/page.tsx
+++ app/page.tsx
@@ -294,7 +294,7 @@ export default function Home() {
                               d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
                           </svg>
-                          <span>Created {formatTimestamp(workflow.createdAt)}</span>
+                          <span>Started {formatTimestamp(workflow.createdAt)}</span>
                         </div>
                       </div>
                       <div className="ml-6">
